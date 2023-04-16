bfe3e412ffcd208610aff158e30cf3fea9ee0e98
Marco Bonici <bonici.marco@gmail.com>
2023-04-16 22:05:25 +0200

    Removed useless file

--- scripts/job.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/home/mbonici/julia-1.9.0-rc2/bin/julia -t 32 mcmc.jl
--- scripts/submit.sh
+++
@@ -1,1 +0,0 @@
-bsub -P c7 -q medium -o ciccio.out -e ciccio.err -n 32 -M 10000 /home/mbonici/blinded_challenge_analysis/scripts/job.sh
