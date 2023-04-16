394ff0c9fc592469528615b6c17e1ebd2da75689
Marco Bonici <bonici.marco@gmail.com>
2023-04-16 11:33:17 +0200

    Fixed name in submit job

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,1 +1,1 @@
-bsub -P c7 -q medium -o ciccio.out -e ciccio.err -n 32 -M 10000 /home/mbonici/blinded_challenge_analysis/scripts/lol.sh
+bsub -P c7 -q medium -o ciccio.out -e ciccio.err -n 32 -M 10000 /home/mbonici/blinded_challenge_analysis/scripts/job.sh
